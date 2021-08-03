3e77213ba32dbac0b141c2939cf208cb7fe26a73
Kaan Sahin <kaan.sahin@active-group.de>
2021-08-03 17:38:05 +0200

    Add string_switch

---
+++ string_switch/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to string_switch
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ string_switch/src/string_switch/core.clj
@@ -0,0 +1,77 @@
+(ns string-switch.core)
+
+(comment
+  (defn prepare-statement [thing statement]
+    `((= ~thing ~(first statement))
+      (second '~statement)))
+
+  (defmacro switch
+    [thing & statements]
+    (let [prepared-statements (mapcat (fn [s] (prepare-statement thing s))
+                                      statements)]
+      `(cond
+         ~@prepared-statements
+
+         :else
+         (throw (Exception. (str "No match, got: " ~thing))))))
+
+  (let [y "Work"]
+    (switch y
+            ("Holiday" "I am not around")
+            ( "Work" "How can I help?"))))
+
+
+(comment
+  (defn prepare-statement [thing statement]
+    `((= ~thing ~(first statement))
+      (second '~statement)))
+
+
+  (defn prepare-more [thing statement]
+    (let [[case thingy arrow then] statement]
+      (prepare-statement thing (list thingy then))))
+
+  (defmacro switch
+    [thing & statements]
+    (let [prepared-statements (mapcat (fn [s] (prepare-more thing s))
+                                      statements)]
+      `(cond
+         ~@prepared-statements
+
+         :else
+         (throw (Exception. (str "No match, got: " ~thing))))))
+
+  (let [y "Work"]
+    (switch y
+            (case "Holiday" -> "I am not around")
+            (case "Work" -> "How can I help?"))))
+
+
+
+(defn prepare-statement [thing statement]
+  `((= ~thing ~(first statement))
+    (second '~statement)))
+
+
+(defn prepare-more [thing statement]
+  (let [[case thingy arrow then] statement]
+    (prepare-statement thing (list thingy then))))
+
+(defn prepare-statements [s]
+  (partition 4 s))
+
+(defmacro switch
+  [thing & statements]
+  (let [prepared-statements (mapcat (fn [s] (prepare-more thing s))
+                                    (prepare-statements statements))]
+    `(cond
+       ~@prepared-statements
+
+       :else
+       (throw (Exception. (str "No match, got: " ~thing))))))
+
+(let [y "Work"]
+  (switch y
+          case "Holiday" -> "I am not around"
+          case "Work" -> "How can I help?"
+          case "Night" -> "zzZZzzzzZZ"))
