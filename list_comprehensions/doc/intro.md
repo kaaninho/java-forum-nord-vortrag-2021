a56b2520d376b66b13c19aca8a2e2c9d64f2bbca
Kaan Sahin <kaan.sahin@active-group.de>
2021-08-12 10:24:34 +0200

    Add list comprehensions

---
+++ list_comprehensions/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to list_comprehensions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ list_comprehensions/src/list_comprehensions/core.clj
@@ -0,0 +1,56 @@
+(ns list-comprehensions.core)
+
+(comment
+
+  ;; Ziel
+  ;; (lc [(* x x) for x in [1 2 3 4] where (> x 2)])
+
+  (defmacro lc
+    [form]
+    (let [sym      (first form)
+          variable (nth form 2)
+          lis      (nth form 4)
+          pred     (nth form 6)]
+      `~lis)))
+
+
+(comment
+  ;; Ziel
+  ;; (lc [x for x in [1 2 3 4] where (> x 2)])
+
+
+  (defn make-pred-fn
+    [sym pred]
+    `(fn [~sym]
+       ~pred))
+
+  (defmacro lc
+    [form]
+    (let [sym      (first form)
+          variable (nth form 2)
+          lis      (nth form 4)
+          pred     (nth form 6)]
+      `(filter ~(make-pred-fn variable pred)
+               ~lis))))
+
+(defn make-pred-fn
+  [sym pred]
+  `(fn [~sym]
+     ~pred))
+
+(defn make-map-fn
+  [sym fn-form]
+  `(fn [~sym]
+     ~fn-form))
+
+(defmacro lc
+  [form]
+  (let [sym      (first form)
+        variable (nth form 2)
+        lis      (nth form 4)
+        pred     (nth form 6)]
+    `(map ~(make-map-fn variable sym)
+          (filter ~(make-pred-fn variable pred)
+                  ~lis))))
+
+(lc [(* x x) for x in [1 2 3 4] where (> x 2)])
