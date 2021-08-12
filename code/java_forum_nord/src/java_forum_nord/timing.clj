82fae9c65d1c7e9c322ab541223ece406e77f9f0
Kaan Sahin <kaan.sahin@active-group.de>
2021-08-12 12:38:17 +0200

    Improve timing message

--- code/java_forum_nord/src/java_forum_nord/timing.clj
+++ code/java_forum_nord/src/java_forum_nord/timing.clj
@@ -8,7 +8,7 @@
 (let [before (System/currentTimeMillis)
       result (do-something)
       after  (System/currentTimeMillis)]
-  (println "ms: " (- after before))
+  (println "Needed: " (- after before) " ms.")
   result)
 
 
@@ -16,7 +16,7 @@
   `(let [before# (System/currentTimeMillis)
          result#  ~form
          after#  (System/currentTimeMillis)]
-     (println "ms: " (- after# before#))
+     (println "Needed: " (- after# before#) " ms.")
      result#))
 
 (timing (do-something))
