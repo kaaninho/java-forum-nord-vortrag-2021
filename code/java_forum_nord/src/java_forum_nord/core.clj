7a69a354d88d9180cd45eb8c8bf0a99c6ec79d7c
Kaan Sahin <kaan.sahin@active-group.de>
2021-09-08 15:13:03 +0200

    Update
    Clojure Einführung Syntax

--- code/java_forum_nord/src/java_forum_nord/core.clj
+++ code/java_forum_nord/src/java_forum_nord/core.clj
@@ -1,7 +1,56 @@
-(ns java-forum-nord.core
-  (:gen-class))
+(ns java-forum-nord.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;; Kommentar
+
+;; Ausdrücke
+
+3
+
+"String"
+
+'symbol
+
+true
+
+(list 1 2 3)
+
+(inc 3)
+
+(if (> 2 3)
+  "It is true"
+  100)
+
+;; Definition eines "Counters" (der keiner ist)
+(def counter 15)
+
+;; erhöhe um 1
+(inc counter)
+
+;; immer noch 15 -> immutable data
+counter
+
+;; Funktionen
+
+(defn add1
+  [x]
+  (+ 1 x))
+
+(defn factorial
+  [n]
+  (if (= n 1)
+    1
+    (* n (factorial (dec n)))))
+
+;; lokale Bindungen
+
+(let [x 5
+      y 7]
+  (+ x y))
+
+
+;;; ENDE Syntax, Definitionen
+
+(defn do-smth [x y]
+  (if (> x 0)
+    "Ja"
+    y))
